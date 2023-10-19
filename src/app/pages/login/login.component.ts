b86463e7f8f4d80344b232b9ec8568262e3e5b0d
Bruno Pierri <pierri95@gmail.com>
2023-10-18 22:38:38 -0300

    add and create pages of login and register

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoginUserService } from 'src/app/service/login/login-user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit{
+  
+  protected emailPageRegister: String = '';
+  protected formulario: FormGroup|any;
+  protected invalidCredentials: boolean = false;
+  protected invalidCredentialsMessage: String = 'E-mail ou senha incorreto.';
+  protected requestSpinner: boolean = false;
+  
+  constructor(
+    private formBuilder: FormBuilder, 
+    private activedRouter: ActivatedRoute,
+    private router: Router,
+    private loginUserService: LoginUserService,
+  ) {
+
+    localStorage.removeItem('token');
+
+    if(localStorage.getItem('token')){
+      this.router.navigate(['/home']);
+    }
+
+  }
+
+  ngOnInit(): void {
+
+    this.activedRouter.queryParams.subscribe( (params) => {
+      this.emailPageRegister = params['email'];
+    });
+
+    this.formulario = this.formBuilder.group({
+      email: [this.emailPageRegister, Validators.required],
+      password: ['', Validators.required]
+    });
+    
+  }
+
+  sendLogin(): void {
+
+    this.requestSpinner = true;
+
+    const objectData = {
+      username: this.formulario.value.email,
+      password: this.formulario.value.password
+    }
+
+    const requestLogin = this.loginUserService.postLoginUser(objectData);
+
+    requestLogin.subscribe({
+      next: (res: any) => {
+        localStorage.setItem('token', res.token);
+
+        this.router.navigate(['/home']);
+        this.requestSpinner = false;
+      },
+      error: (err: any) => {
+        this.requestSpinner = false;
+        console.log(err);
+        
+        this.invalidCredentials = err.status === 401 ? true : false;
+
+      }
+    });
+
+
+  }
+
+}
---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,92 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegisterUserService } from 'src/app/service/register/register-user.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit{
+
+  private urlApi = 'https://localhost:8000';
+  protected requestSpinner: boolean = false;
+  protected errorMinusculoEmail: boolean = false;
+  protected emailExists: boolean = false;
+  protected formulario: FormGroup|any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private registerUserService: RegisterUserService,
+    private router: Router
+  ) {
+    
+    if(localStorage.getItem('token')){
+      this.router.navigate(['/home']);
+    }
+
+  }
+
+  ngOnInit(): void {
+
+    this.formulario = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+    
+  }
+
+  sendPostRequest(): void{   
+
+    const name: string = this.formulario.value.name;
+    const email: string = this.formulario.value.email;
+    const password: string = this.formulario.value.password;
+
+    const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    const resultExpression: boolean = expression.test(email);
+
+    if(!resultExpression){
+      this.formulario.get('email').errors = true;
+      this.requestSpinner = false;
+      return;
+    }
+
+    if(email !== email.toLowerCase()){
+      this.errorMinusculoEmail = true;
+      return;
+    } else {
+      this.errorMinusculoEmail = false;
+    }
+
+    this.requestSpinner = true;
+    this.emailExists = false; 
+
+    const jsonData = {
+      "name": name,
+      "email": email,
+      "password": password
+    }
+
+    const requestPost = this.registerUserService.postRegisterUser(jsonData);
+
+    requestPost.subscribe({
+      next: (res: any) => {
+        this.requestSpinner = false;
+        this.router.navigate(['/login'], {queryParams: {email: res.email} });
+      }, 
+      error: (err) => {
+        this.requestSpinner = false;
+
+        if(err.status === 400){
+          this.emailExists = true;
+        }
+      }
+    });
+
+  }
+
+
+}
